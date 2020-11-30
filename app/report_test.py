be9069ef14c56eb831619cc5c4e7d337658208b1
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-30 10:58:45 +0300

    initialize blueprint

--- app/report_test.py
+++ app/report_test.py
@@ -11,7 +11,7 @@ class SourceTest(unittest.TestCase):
         '''
         Setting up method that will run before every Test
         '''
-        self.new_source =('Kenyans debts still flactuating','A thrilling drift in kenyan economy','https://techcrunch.com/wp-content/uploads/2020/11/GettyImages-887657568.jpg?w=600',8.5,2000)
+        self.new_source =(1234,'Kenyans debts still flactuating','A thrilling drift in kenyan economy','https://techcrunch.com/wp-content/uploads/2020/11/GettyImages-887657568.jpg?w=600',8.5,2000)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,))
