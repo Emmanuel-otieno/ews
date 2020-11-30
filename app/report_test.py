3d0c8808c26d5499fb3a27947088707e34e0c5dd
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-30 13:37:09 +0300

    deployment to heroku

--- app/report_test.py
+++ app/report_test.py
@@ -1,7 +1,7 @@
 import unittest
 from models import  Report
 
-
+Sources = Report.Sources
 class SourceTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the News class
@@ -11,10 +11,10 @@ class SourceTest(unittest.TestCase):
         '''
         Setting up method that will run before every Test
         '''
-        self.new_source =(1234,'Kenyans debts still flactuating','A thrilling drift in kenyan economy','https://techcrunch.com/wp-content/uploads/2020/11/GettyImages-887657568.jpg?w=600',8.5,2000)
+        self.new_source =Report(1234,'Kenyans debts still flactuating','A thrilling drift in kenyan economy','https://techcrunch.com/wp-content/uploads/2020/11/GettyImages-887657568.jpg?w=600',8.5,2000)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_source,))
+        self.assertTrue(isinstance(self.new_source,Report))
 
 
 if __name__ == '__main__':
